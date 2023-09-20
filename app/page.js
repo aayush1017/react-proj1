76d352a5f6b5101b6398bdc6db51b8fa01c7085d
aayush1017 <d.aayushjain@gmail.com>
2023-09-21 03:34:31 +0530

    React Basics

--- app/page.js
+++ app/page.js
@@ -1,9 +1,56 @@
-import React from 'react'
+'use client';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 const page = () => {
-  return (
-    <div>Hello World</div>
-  )
-}
+  const [imagesData, setImagesData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    getImages();
+  }, []);
+  const getImages = async () => {
+    // setLoading(true);
+    try {
+      const res = await axios.get('https://picsum.photos/v2/list');
+      setImagesData(res.data);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
 
-export default page
+  return (
+    <div className="flex justify-center items-center">
+      {loading ? (
+        <div class="flex justify-center items-center min-h-screen">
+          <div class="loader ease-linear rounded-full border-4 border-t-4 border-gray-200 h-12 w-12"></div>
+        </div>
+      ) : (
+        <>
+          <div className="p-10">
+            {imagesData?.map((item, idx) => {
+              return (
+                <img
+                  key={idx}
+                  src={item.download_url}
+                  width={300}
+                  height={300}
+                  className="m-10 rounded inline-block"
+                />
+              );
+            })}
+          </div>
+          {/* <button
+            className="p-5 py-3 bg-gray-500 rounded flex text-white"
+            onClick={getImages}
+          >
+            Get Images
+          </button> */}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default page;
